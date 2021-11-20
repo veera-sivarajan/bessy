f157627792e3cc60d3d21e854a74b865fdfce8a6
Veera <sveera.2001@gmail.com>
2021-11-20 11:43:05 -0500

    Print instructions

---
+++ src/chunk.rs
@@ -0,0 +1,21 @@
+pub enum Opcode {
+    Return,
+    Add,
+    Sub,
+}
+
+pub struct Chunk {
+    pub code: Vec<Opcode>,
+}
+
+impl Chunk {
+    pub fn new() -> Chunk {
+        Chunk {
+            code: Vec::new(),
+        }
+    }
+        
+    pub fn write(&mut self, instruction: Opcode) {
+        self.code.push(instruction);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+mod chunk;
+use crate::chunk::Chunk;
+use crate::chunk::Opcode;
+
+fn disassemble_chunk(chunk: Chunk, name: &str) {
+    println!("== {} ==", name);
+    for (offset, instruction) in chunk.code.iter().enumerate() {
+        print!("{} ", offset);
+        print_instruction(instruction);
+    }
+    println!("== END ==");
+}
+    
+fn print_instruction(instruction: &Opcode) {
+    match instruction {
+        Opcode::Return => println!("OP_RETURN"),
+        Opcode::Add    => println!("OP_ADD"),
+        Opcode::Sub    => println!("OP_SUB"),
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    // println!("Hello, world!");
+    let mut program = Chunk::new();
+    program.write(Opcode::Return);
+    program.write(Opcode::Add);
+    program.write(Opcode::Sub);
+    disassemble_chunk(program, "test chunk");
 }

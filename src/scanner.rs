9a4b96803e0fbc564e50f1cd972beadd43237933
Veera <sveera.2001@gmail.com>
2021-12-21 11:57:12 -0500

    Scan numbers, identifiers and keywords

--- src/scanner.rs
+++ src/scanner.rs
@@ -2,36 +2,43 @@ use crate::{token::{Token, TokenType}};
 
 pub struct Scanner<'a> {
     source: &'a str,
-    chars: Vec<char>,
     start: usize,
     current: usize,
-    pub line: u32,
+    line: u32,
 }
 
 impl Scanner {
     pub fn init_scanner(source: &str) -> Scanner {
-        let chars: Vec<char> = source.chars().collect();
-        Scanner { source, chars, start: 0, current: 0, line: 1 }
+        Scanner { source, start: 0, current: 0, line: 1 }
     }
 
     fn is_at_end(&self) -> bool {
-        self.source.as_bytes()[self.current] == b'\0'
+        self.current == self.source.len()
     }
 
-    fn advance(&mut self) -> char {
+    fn advance(&mut self) -> u8 {
+        let curr_char = self.peek();
         self.current += 1;
-        self.chars[self.current - 1]
+        curr_char
     }
 
-    fn peek(&self) -> char {
-        self.chars[self.current]
+    fn peek(&self) -> u8 {
+        if self.is_at_end() {
+            0
+        } else {
+            self.source.as_bytes()[self.current]
+        }
     }
 
-    fn peek_next(&self) -> char {
-        self.chars[self.current + 1]
+    fn peek_next(&self) -> u8 {
+        if self.current > self.code.len() - 2 {
+            b'\0'
+        } else {
+            self.source.as_bytes()[self.current + 1]
+        }
     }
 
-    fn matches(&mut self, expected: char) -> bool {
+    fn matches(&mut self, expected: u8) -> bool {
         if self.is_at_end() || self.peek() != expected {
             false
         } else {
@@ -48,13 +55,9 @@ impl Scanner {
                     self.line += 1;
                     self.advance() += 1;
                 },
-                '/' => {
-                    if self.peek_next() == '/' {
-                        while self.peek() != '\n' && !self.is_at_end() {
-                            self.advance();
-                        }
-                    } else {
-                        return
+                b'/' if self.peek_next() == b'/' => {
+                    while self.peek() != '\n' && !self.is_at_end() {
+                        self.advance();
                     }
                 }
                 _ => return
@@ -69,61 +72,77 @@ impl Scanner {
         if self.is_at_end() {
             return self.make_token(TokenType::Eof);
         } else {
-            let c = self.advance();
-            match c {
-                '(' => self.make_token(TokenType::LeftParen),
-                ')' => self.make_token(TokenType::RightParen),
-                '{' => self.make_token(TokenType::LeftBrace),
-                '}' => self.make_token(TokenType::RightBrace),
-                ';' => self.make_token(TokenType::Semicolon),
-                ',' => self.make_token(TokenType::Comma),
-                '.' => self.make_token(TokenType::Dot),
-                '-' => self.make_token(TokenType::Minus),
-                '+' => self.make_token(TokenType::Plus),
-                '/' => self.make_token(TokenType::Slash),
-                '*' => self.make_token(TokenType::Star),
-                '!' => {
-                    let new_type = if self.matches('=') {
+            match self.advance() {
+                b'(' => self.make_token(TokenType::LeftParen),
+                b')' => self.make_token(TokenType::RightParen),
+                b'{' => self.make_token(TokenType::LeftBrace),
+                b'}' => self.make_token(TokenType::RightBrace),
+                b';' => self.make_token(TokenType::Semicolon),
+                b',' => self.make_token(TokenType::Comma),
+                b'.' => self.make_token(TokenType::Dot),
+                b'-' => self.make_token(TokenType::Minus),
+                b'+' => self.make_token(TokenType::Plus),
+                b'/' => self.make_token(TokenType::Slash),
+                b'*' => self.make_token(TokenType::Star),
+                b'!' => {
+                    let new_type = if self.matches(b'=') {
                         TokenType::BangEqual
                     } else {
                         TokenType::Bang
                     }
                     self.make_token(new_type)
                 },
-                '=' => {
-                    let new_type = if self.match('=') {
+                b'=' => {
+                    let new_type = if self.match(b'=') {
                         TokenType::EqualEqual
                     } else {
                         TokenType::Equal
                     }
                     self.make_token(new_type)
                 },
-                '<' => {
-                    let new_type = if self.match('=') {
+                b'<' => {
+                    let new_type = if self.match(b'=') {
                         TokenType::LessEqual
                     } else {
                         TokenType::Less
                     }
                     self.make_token(new_type)
                 },
-                '>' => {
-                    let new_type = if self.match('=') {
+                b'>' => {
+                    let new_type = if self.match(b'=') {
                         TokenType::GreaterEqual
                     } else {
                         TokenType::Greater
                     }
                     self.make_token(new_type)
                 },
-                '"' => self.scan_string(),
+                b'"' => self.scan_string(),
+                c if c.is_ascii_digit() => self.scan_number(),
+                c if c.is_ascii_alphabetic() || c == b'_' => self.identifier(),
+                _ => self.error_token("Unexpected character."),
+            }
+        }
+    }
+
+    fn scan_number(&mut self) -> Token {
+        while self.peek().is_ascii_digit() {
+            self.advance();
+        }
+
+        if self.peek() == b'.' && self.peek_next().is_ascii_digit() {
+            self.advance();
+            
+            while self.peek().is_ascii_digit() {
+                self.advance();
             }
         }
 
-        return self.error_token("Unexpected character.");
+        self.make_token(TokenType::Number)
     }
 
     fn scan_string(&mut self) -> Token {
-        while self.peek() != '"' && !self.is_at_end() {
-            if self.peek() == '\n' {
+        while self.peek() != b'"' && !self.is_at_end() {
+            if self.peek() == b'\n' {
                 self.line += 1;
             }
             self.advance();
@@ -137,6 +156,49 @@ impl Scanner {
         }
     }
 
+    fn identifier(&mut self) -> Token {
+        while self.peek().is_ascii_alphabetic() || self.peek().is_ascii_digit() {
+            self.advance();
+        }
+        self.make_token(self.identifier_type());
+    }
+
+    fn identifier_type(&mut self) -> TokenType {
+        match self.source.as_bytes()[self.start] {
+            b'a' => self.check_rest(1, 2, "nd", TokenType::And),
+            b'e' => self.check_rest(1, 3, "lse", TokenType::Else),
+            b'i' => self.check_rest(1, 1, "f", TokenType::If),
+            b'n' => self.check_rest(1, 2, "il", TokenType::Nil),
+            b'o' => self.check_rest(1, 1, "r", TokenType::Or),
+            b'p' => self.check_rest(1, 4, "rint", TokenType::Print),
+            b'r' => self.check_rest(1, 5, "eturn", TokenType::Return),
+            b'v' => self.check_rest(1, 2, "ar", TokenType::Var),
+            b'w' => self.check_rest(1, 4, "hile", TokenType::While),
+            b't' => self.check_rest(1, 3, "rue", TokenType::True),
+            b'f' => {
+                match self.source.as_bytes()[self.start + 1] {
+                    b'a' => self.check_rest(2, 3, "lse", TokenType::False),
+                    b'o' => self.check_rest(2, 1, "r", TokenType::For),
+                    b'u' => self.check_rest(2, 1, "n", TokenType::Fun),
+                    _ => TokenType::Identifier,
+                }
+            },
+            _ => TokenType::Identifier,
+        }
+    }
+
+    fn check_rest(&self,
+                  start: usize,
+                  len: usize,
+                  rest: &str, kind: TokenType) -> TokenType {
+        let lexeme = &self.source[self.start..start.current];
+        if lexeme.len() == start + len && lexeme == rest {
+            kind
+        } else {
+            TokenType::Identifier
+        }
+    }
+        
     fn make_token(&self, kind: TokenType) -> Token {
         Token {
             kind,
@@ -145,7 +207,7 @@ impl Scanner {
         }
     }
 
-    fn error_token<'c>(&self, message: &<'c> str) -> Token<'c> {
+    fn error_token(&self, message: &'static str) -> Token<'static> {
         Token {
             kind: TokenType::Error,
             lexeme: message,

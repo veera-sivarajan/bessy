7da5fd24aceeb90f827ba8478ac1f04869e04b20
Veera <sveera.2001@gmail.com>
2022-06-14 14:27:03 -0400

    Scan two-character tokens and skip needless characters

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
+#[macro_use]
+mod error;
 mod token;
 mod scanner;
 
 use std::fs;
 
 fn main() {
-    let contents = fs::read_to_string("test/scan.lox").unwrap();
+    // let contents = fs::read_to_string("test/scan.lox").unwrap();
+    let contents = String::from("        ()");
     let mut scanner = scanner::Scanner::new(&contents);
+    println!("{:?}", scanner.scan_token());
+    println!("{:?}", scanner.scan_token());
+    println!("{:?}", scanner.scan_token());
+    println!("{:?}", scanner.scan_token());
     println!("{:?}", scanner.scan_token());
     println!("{:?}", scanner.scan_token());
 }
--- src/scanner.rs
+++ src/scanner.rs
@@ -1,12 +1,44 @@
 use crate::token::{Token, TokenType};
+use crate::error::BessyError;
+use std::iter::SkipWhile;
+use std::str::Bytes;
 
 // Takes a heap allocated string and generates tokens on demand. 
+
+type Result<T> = std::result::Result<T, BessyError>;
 
 pub struct Scanner<'a> {
     source: &'a str, 
     start: usize,
     current: usize,
     line: u16,
+}
+
+trait Needless {
+    fn is_needless(&self) -> bool;
+}
+
+impl Needless for u8 {
+    fn is_needless(&self) -> bool {
+        match self {
+            b' '|b'\r'|b'\t' => true,
+            _ => false,
+        }
+    }
+}
+        
+
+impl<'a> Iterator for Scanner<'a> {
+    type Item = u8;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if !self.is_at_end() {
+            self.current += 1;
+            Some(self.source.as_bytes()[self.current - 1])
+        } else {
+            None
+        }
+    }
 }
 
 impl<'a> Scanner<'a> {
@@ -18,23 +50,57 @@ impl<'a> Scanner<'a> {
         self.current >= self.source.len()
     }
 
-    fn advance(&mut self) -> u8 {
-        self.current += 1;
-        self.source.as_bytes()[self.current - 1]
+    fn next_eq(&mut self, expected: u8) -> bool {
+        self.peekable().next_if_eq(&expected).is_some()
     }
 
-    pub fn scan_token(&mut self) -> Token {
+    pub fn scan_token(&mut self) -> Result<Token> {
         self.start = self.current;
 
         if self.is_at_end() {
-            Token::new(TokenType::Eof, self.line)
+            Ok(Token::new(TokenType::Eof, self.line))
         } else {
-            match self.advance() {
-                b'(' => Token::new(TokenType::LeftParen, self.line),
-                b')' => Token::new(TokenType::LeftParen, self.line),
-                b'{' => Token::new(TokenType::LeftBrace, self.line),
-                b'}' => Token::new(TokenType::RightBrace, self.line),
-                _ => Token::new(TokenType::Unknown, self.line),
+            match self.skip_while(|c| c.is_needless()).next().unwrap() {
+                b'(' => Ok(Token::new(TokenType::LeftParen, self.line)),
+                b')' => Ok(Token::new(TokenType::RightParen, self.line)),
+                b'{' => Ok(Token::new(TokenType::LeftBrace, self.line)),
+                b'}' => Ok(Token::new(TokenType::RightBrace, self.line)),
+                b';' => Ok(Token::new(TokenType::Semicolon, self.line)),
+                b',' => Ok(Token::new(TokenType::Comma, self.line)),
+                b'.' => Ok(Token::new(TokenType::Dot, self.line)),
+                b'-' => Ok(Token::new(TokenType::Minus, self.line)),
+                b'+' => Ok(Token::new(TokenType::Plus, self.line)),
+                b'/' => Ok(Token::new(TokenType::Slash, self.line)),
+                b'*' => Ok(Token::new(TokenType::Star, self.line)),
+                b'!' => {
+                    if self.next_eq(b'=') {
+                        Ok(Token::new(TokenType::BangEqual, self.line))
+                    } else {
+                        Ok(Token::new(TokenType::Bang, self.line))
+                    }
+                },
+                b'=' => {
+                    if self.next_eq(b'=') {
+                        Ok(Token::new(TokenType::EqualEqual, self.line))
+                    } else {
+                        Ok(Token::new(TokenType::Equal, self.line))
+                    }
+                },
+                b'<' => {
+                    if self.next_eq(b'=') {
+                        Ok(Token::new(TokenType::LessEqual, self.line))
+                    } else {
+                        Ok(Token::new(TokenType::Less, self.line))
+                    }
+                },
+                b'>' => {
+                    if self.next_eq(b'=') {
+                        Ok(Token::new(TokenType::GreaterEqual, self.line))
+                    } else {
+                        Ok(Token::new(TokenType::Greater, self.line))
+                    }
+                },
+                _ => scan_error!()
             }
         }
     }

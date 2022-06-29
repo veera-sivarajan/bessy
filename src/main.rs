4ae0a10fab814b18a65ff442f9d17baa48fce2fa
Veera <sveera.2001@gmail.com>
2022-06-29 15:53:42 -0400

    Test compiling expression from multiple lines

--- src/main.rs
+++ src/main.rs
@@ -10,9 +10,10 @@ mod chunk;
 // (add-hook 'after-save-hook 'rust-compile) 
 // (setq compilation-scroll-output 'first-error) 
 
+use std::fs;
 fn main() {
-    // let contents = fs::read_to_string("test/scan.lox").unwrap();
-    let contents = String::from("1 + ((-1 + -1) * 90)");
+    let contents = fs::read_to_string("test/scan.lox").unwrap();
+    // let contents = String::from("1 + ((-1 + -1) * 90)");
     // let mut scanner = lexer::Lexer::new(&contents);
     // for _i in 0..15 {
     //     println!("Token: {:?}", scanner.next_token());

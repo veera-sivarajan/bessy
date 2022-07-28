501bea590d6336ed11e440480f298153ba00cd10
Veera <sveera.2001@gmail.com>
2022-07-28 00:52:51 -0400

    Add a run button

--- wasm/www/index.js
+++ wasm/www/index.js
@@ -7,11 +7,44 @@ import * as wasm from "wasm";
 
 import {EditorState, EditorView, basicSetup} from "@codemirror/basic-setup"
 import {javascript} from "@codemirror/lang-javascript"
+import {oneDark, oneDarkTheme, oneDarkHighlightStyle} from "@codemirror/theme-one-dark"
 
+let output = document.getElementById('output');
+let timer;
 let editor = new EditorView({
-  state: EditorState.create({
-    extensions: [basicSetup, javascript()]
-  }),
-  parent: document.body
+    state: EditorState.create({
+        extensions: [
+            basicSetup,
+            javascript(), 
+            oneDark,
+            oneDarkTheme,
+            oneDarkHighlightStyle.extension,
+            EditorView.updateListener.of((v)=> {
+                if(v.docChanged) {
+                    if(timer) clearTimeout(timer);
+                    timer = setTimeout(() => {
+                        var y = document.createTextNode("This just got added");
+                        output.appendChild(y);
+                    }, 500 );
+                }
+            })
+        ],
+        doc:`var i = 10;
+var b = 2;
+print a + b;
+
+for (var i = 0; i < 10; i = i + 1) {
+    print i;
+}
+
+var a = 0;
+var e = 10;
+while (a < e) {
+    print a;
+    a = a + 1;
+}
+`,
+    }),
+    parent: document.getElementById('source-code'),
 })
 

ea749068a6334d8f6ffc5e3f2d99aa01823b85e0
Veera <sveera.2001@gmail.com>
2022-07-29 14:20:08 -0400

    Add a binary crate for compiler

---
+++ by/src/main.rs
@@ -0,0 +1,41 @@
+use bessy;
+use std::io;
+
+
+pub struct WasmPrinter {
+    buffer: String,
+}
+
+impl WasmPrinter {
+    pub fn new() -> Self {
+        Self { buffer: String::new() }
+    }
+
+    fn to_string(&self) -> String {
+        self.buffer.clone()
+    }
+}
+
+impl std::io::Write for WasmPrinter {
+    fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
+        let write_str = match std::str::from_utf8(buf) {
+            Ok(v) => v,
+            Err(e) => panic!("Invalid utf-8 sequence."),
+        };
+        self.buffer.push_str(write_str);
+        Ok(buf.len())
+    }
+
+    fn flush(&mut self) -> std::io::Result<()> {
+        self.buffer.clear();
+        Ok(())
+    }
+}
+
+
+fn main() {
+    let input = "print 1 + 1;".to_string();
+    let mut output = WasmPrinter::new();
+    bessy::evaluate(input, &mut output);
+    print!("{}", output.to_string());
+}

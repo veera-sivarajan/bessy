08a1c5686c11e91191601ad213107e05fa2b01a6
Veera <sveera.2001@gmail.com>
2023-05-27 20:26:21 -0400

    Inline all structs in Expr enum

--- core/src/expr.rs
+++ core/src/expr.rs
@@ -2,55 +2,34 @@ use crate::lexer::Token;
 
 #[derive(Clone, Debug)]
 pub enum Expr {
-    Variable(VariableExpr),
-    Binary(BinaryExpr),
-    Unary(UnaryExpr),
-    Number(NumberExpr),
+    Variable {
+        name: Token,
+    },
+    Binary {
+        left: Box<Expr>,
+        oper: Token,
+        right: Box<Expr>,
+    },
+    Unary {
+        oper: Token,
+        right: Box<Expr>,
+    },
+    Number {
+        value: f64,
+    },
     Boolean(bool),
     String(String),
-    Assign(AssignExpr),
-    Group(GroupExpr),
-    Call(CallExpr),
+    Assign {
+        name: Token,
+        value: Box<Expr>,
+    },
+    Group {
+        expr: Box<Expr>,
+    },
+    Call {
+        callee: Box<Expr>,
+        paren: Token,
+        args: Vec<Expr>,
+    },
     Nil,
-}
-
-#[derive(Clone, Debug)]
-pub struct GroupExpr {
-    pub expr: Box<Expr>,
-}
-
-#[derive(Clone, Debug)]
-pub struct BinaryExpr {
-    pub left: Box<Expr>,
-    pub oper: Token,
-    pub right: Box<Expr>,
-}
-
-#[derive(Clone, Debug)]
-pub struct UnaryExpr {
-    pub oper: Token,
-    pub right: Box<Expr>,
-}
-
-#[derive(Clone, Debug)]
-pub struct NumberExpr {
-    pub value: f64,
-}
-
-#[derive(Clone, Debug)]
-pub struct VariableExpr {
-    pub name: Token,
-}
-
-#[derive(Clone, Debug)]
-pub struct AssignExpr {
-    pub name: Token,
-    pub value: Box<Expr>,
-}
-
-#[derive(Clone, Debug)]
-pub struct CallExpr {
-    pub callee: Box<Expr>,
-    pub paren: Token,
-    pub args: Vec<Expr>,
 }
